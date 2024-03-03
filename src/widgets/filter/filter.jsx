f9b9c72a2db5228ab3902de89d06ddf6cd32d4a0
pranayjoshi <pranayjoshi4466@gmail.com>
2024-03-02 19:17:29 -0600

    added Ui components

--- src/widgets/filter/filter.jsx
+++ src/widgets/filter/filter.jsx
@@ -1,7 +1,12 @@
-export default function Filter(){
-    return (
-        <div className="h-full w-1/5 items-center border border-gray-500 bg-white rounded-lg">
-            <h1 className="text-center text-3xl text-semibold py-4">Filter</h1>
-        </div>
-    )
+import { Combobox } from "@/components/ui/combobox";
+
+export default function Filter() {
+  return (
+    <div className="h-full w-1/5 items-center justify-center place-items-center border border-gray-300 p-4 bg-white rounded-lg">
+      <h1 className="text-center text-3xl font-semibold py-2 mb-4">Filter</h1>
+      <div className="flex items-center justify-center">
+        <Combobox />
+      </div>
+    </div>
+  );
 }
